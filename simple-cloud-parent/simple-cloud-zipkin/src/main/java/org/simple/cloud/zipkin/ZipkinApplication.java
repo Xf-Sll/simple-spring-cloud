aa1b62337a630bc87245f99dca425a39e3877327
xufan <xufan@winchannel.net>
2017-03-28 17:39:51 +0800

    集成服务调用追踪，并测试成功

--- simple-cloud-parent/simple-cloud-zipkin/src/main/java/org/simple/cloud/zipkin/ZipkinApplication.java
+++ simple-cloud-parent/simple-cloud-zipkin/src/main/java/org/simple/cloud/zipkin/ZipkinApplication.java
@@ -2,9 +2,11 @@ package org.simple.cloud.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import zipkin.server.EnableZipkinServer;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableZipkinServer
 public class ZipkinApplication {

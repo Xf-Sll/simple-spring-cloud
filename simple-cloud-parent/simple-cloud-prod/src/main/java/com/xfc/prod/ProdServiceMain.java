22a4816537fdc86b320d0f8baf91b72728060db3
xufan <xufan@winchannel.net>
2017-03-28 16:38:01 +0800

    为其他服务增加阻断和http模板技术

--- simple-cloud-parent/simple-cloud-prod/src/main/java/com/xfc/prod/ProdServiceMain.java
+++ simple-cloud-parent/simple-cloud-prod/src/main/java/com/xfc/prod/ProdServiceMain.java
@@ -1,13 +1,32 @@
 package com.xfc.prod;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
+@EnableCircuitBreaker
 public class ProdServiceMain 
 {
+	
+	private static final Logger log = LoggerFactory.getLogger(ProdServiceMain.class);
+	
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
     public static void main( String[] args )
     {
     	new SpringApplicationBuilder(ProdServiceMain.class).web(true).run(args);
